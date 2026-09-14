fcd0972e46e4b9649a50d869a84abcedb0f831a7
bernard balansag <bernardbalansag01@gmail.com>
2026-09-14 17:56:42 +0800

    phase 2.1C: wire product and mix services into session

--- src/application/session.ts
+++ src/application/session.ts
@@ -2,9 +2,15 @@ import { CalibrationService } from './calibrations/CalibrationService';
 import { InMemoryCalibrationRepository } from './calibrations/InMemoryCalibrationRepository';
 import { InMemoryMaterialRepository } from './materials/InMemoryMaterialRepository';
 import { MaterialService } from './materials/MaterialService';
+import { InMemoryMixPresetRepository } from './mixPresets/InMemoryMixPresetRepository';
+import { MixPresetService } from './mixPresets/MixPresetService';
+import { InMemoryProductRepository } from './products/InMemoryProductRepository';
+import { ProductService } from './products/ProductService';
 
 export const materialRepository = new InMemoryMaterialRepository();
 export const calibrationRepository = new InMemoryCalibrationRepository();
+export const mixPresetRepository = new InMemoryMixPresetRepository();
+export const productRepository = new InMemoryProductRepository();
 
 export const materialService = new MaterialService(materialRepository, async (materialId) => {
   const records = await calibrationRepository.list();
@@ -13,3 +19,9 @@ export const materialService = new MaterialService(materialRepository, async (ma
 });
 
 export const calibrationService = new CalibrationService(calibrationRepository, materialRepository);
+export const productService = new ProductService(productRepository, mixPresetRepository);
+export const mixPresetService = new MixPresetService(
+  mixPresetRepository,
+  materialRepository,
+  productRepository,
+);
