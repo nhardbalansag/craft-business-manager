ca72d5976f85c8b8ec793f43f64690171ad04260
bernard balansag <bernardbalansag01@gmail.com>
2026-09-18 06:38:22 +0800

    Load professional sidebar theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './responsive.css';
 import './appDrawer.css';
+import './appDrawerProfessional.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
